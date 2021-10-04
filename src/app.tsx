52714846f58727908c6249294309bb210e977fe5
leye195 <leye195@naver.com>
2021-10-04 17:11:08 +0900

    # add assets on public directory

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        React App
+      </h1>
+      <div
+        style={{
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div>Sample image</div>
+        <img src="/public/assets/sample.jpg" alt="img" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
